108c5e347b92a03b7a0cdf141f5fbcc997c44fe3
Abhinav-Praveen004 <abhinavpraveen.2024aids@sece.ac.in>
2025-09-08 15:59:32 +0530

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,23 @@
+from flask import current_app as app, render_template, request
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    result = None
+    if request.method == 'POST':
+        num1 = request.form.get('num1', type=float)
+        num2 = request.form.get('num2', type=float)
+        operation = request.form.get('operation')
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = 'Error: Division by zero'
+
+    return render_template('calculator.html', result=result)
